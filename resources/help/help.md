04bee1294174f7fd4363190594c424cb39e63901
YinRuizhe <909510519@qq.com>
2026-03-15 17:52:32 +0800

    helppage

---
+++ resources/help/help.md
@@ -0,0 +1,6 @@
+        {"shortcut":"C","text":"特定区域截图(部分反应)"},
+        {"shortcut":"Shift+C","text":"全软件截图"},
+        {"shortcut":"M","text":"快速添加自慰记录"},
+        {"shortcut":"W","text":"快速添加作品"},
+        {"shortcut":"A","text":"快速添加晨勃记录"},
+        {"shortcut":"L","text":"快速添加做爱记录"}
--- ui/pages/HelpPage.py
+++ ui/pages/HelpPage.py
@@ -1,29 +1,30 @@
-
-from PySide6.QtWidgets import QPushButton, QHBoxLayout,QLabel,QVBoxLayout,QDialog,QFormLayout, QWidget
-from PySide6.QtCore import Signal,Qt
-from config import ICONS_PATH
+from PySide6.QtWidgets import QVBoxLayout, QWidget
+from PySide6.QtCore import Signal
 from PySide6.QtGui import QIcon
-from controller.ShortcutRegistry import ShortcutRegistry
-from darkeye_ui.components.label import Label
+
+from config import ICONS_PATH, HELP_MD_PATH
+from darkeye_ui.components.input import TextEdit
+
 
 class HelpPage(QWidget):
-    #帮助窗口
-    success=Signal(bool)#定义信号
-    def __init__(self,parent=None):
+    # 帮助窗口
+    success = Signal(bool)
+
+    def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle("帮助")
         self.setWindowIcon(QIcon(str(ICONS_PATH / "circle-question-mark.png")))
 
-        datas=[
-        {"shortcut":"C","text":"特定区域截图(部分反应)"},
-        {"shortcut":"Shift+C","text":"全软件截图"},
-        {"shortcut":"M","text":"快速添加自慰记录"},
-        {"shortcut":"W","text":"快速添加作品"},
-        {"shortcut":"A","text":"快速添加晨勃记录"},
-        {"shortcut":"L","text":"快速添加做爱记录"}
-        ]
+        layout = QVBoxLayout(self)
+        self._text = TextEdit()
+        self._text.setReadOnly(True)
+        layout.addWidget(self._text)
 
-        mainlayout=QFormLayout(self)
-        for data in datas:
-            mainlayout.addRow(Label(data["shortcut"]),Label(data["text"]))
-        mainlayout.setLabelAlignment(Qt.AlignRight | Qt.AlignVCenter)  
+        if HELP_MD_PATH.is_file():
+            try:
+                text = HELP_MD_PATH.read_text(encoding="utf-8")
+                self._text.setMarkdown(text)
+            except Exception:
+                self._text.setPlainText("帮助文件无法读取。")
+        else:
+            self._text.setPlainText("帮助文件不存在或无法读取。")
